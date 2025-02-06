5d8017441706462a84aab7bd4444f5f797b76cac
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-02-06 11:45:49 +0300

    Added .env_example file

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -34,7 +34,7 @@ DOMAIN = os.environ['DOMAIN']
 DB_NAME = os.environ['DB_NAME']
 DB_USER = os.environ['DB_USER']
 DB_PASSWORD = os.environ['DB_PASSWORD']
-DB_HOST = os.environ['DB_HOST']
+DB_HOST = os.getenv('DB_HOST', 'db')
 DB_PORT = os.environ['DB_PORT']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
